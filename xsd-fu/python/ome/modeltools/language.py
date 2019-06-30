bad65c88db08d2fc752139f34f8613184933b04d
Roger Leigh <rleigh@codelibre.net>
2019-06-30 14:03:02 +0100

    xsd-fu: Tidy templates into single templates directory

--- xsd-fu/python/ome/modeltools/language.py
+++ xsd-fu/python/ome/modeltools/language.py
@@ -308,7 +308,7 @@ class Java(Language):
         self.type_map['MIMEtype'] = 'String'
 
         self.name = "Java"
-        self.template_dir = "templates-java"
+        self.template_dir = "templates/java"
         self.source_suffix = ".java"
         self.header_suffix = None
         self.converter_name = "MetadataConverter"
@@ -399,7 +399,7 @@ class CXX(Language):
         self.type_map['MIMEtype'] = 'std::string'
 
         self.name = "C++"
-        self.template_dir = "templates-cpp"
+        self.template_dir = "templates/cpp"
         self.source_suffix = ".cpp"
         self.header_suffix = ".h"
         self.converter_name = "Convert"
